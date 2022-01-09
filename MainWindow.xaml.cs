a2296076601380abe716d9a6d0f39b76e894fb86
AfKsenia <afksenia@gmail.com>
2022-01-09 14:56:40 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfL9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> styles = new List<string>() { "Светлая тема", "Темная тема" };
+            styleBox.ItemsSource = styles;
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.SelectedIndex = 0;
+        }
+
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            int styleIndex = styleBox.SelectedIndex;
+            Uri uri = new Uri("Lighttheme.xaml", UriKind.Relative);
+            if (styleIndex == 1)
+            {
+                uri = new Uri("Darktheme.xaml", UriKind.Relative);
+            }
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = (sender as ComboBox).SelectedItem as string;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            int fontSize = Convert.ToInt32((sender as ComboBox).SelectedItem as string);
+            if (textBox != null)
+            {
+                textBox.FontSize = fontSize;
+            }
+        }
+
+        private void Black_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Red_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Red;
+            }
+        }
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Документы Word (*.docx)|*.docx|Документы Word 97-2003 (*.doc)|*.doc|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Документы Word (*.docx)|*.docx|Документы Word 97-2003 (*.doc)|*.doc|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BoldExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.FontWeight == FontWeights.Normal)
+                {
+                    textBox.FontWeight = FontWeights.Bold;
+                    bold.BorderBrush = Brushes.DimGray;
+                }
+                else
+                {
+                    textBox.FontWeight = FontWeights.Normal;
+                    bold.BorderBrush = null;
+                }
+            }
+        }
+
+        private void ItalicExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.FontStyle == FontStyles.Normal)
+                {
+                    textBox.FontStyle = FontStyles.Italic;
+                    italic.BorderBrush = Brushes.DimGray;
+                }
+                else
+                {
+                    textBox.FontStyle = FontStyles.Normal;
+                    italic.BorderBrush = null;
+                }
+            }
+        }
+
+        private void UnderlineExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.TextDecorations == TextDecorations.Underline)
+                {
+                    textBox.TextDecorations = null;
+                    underline.BorderBrush = null;
+                }
+                else
+                {
+                    textBox.TextDecorations = TextDecorations.Underline;
+                    underline.BorderBrush = Brushes.DimGray; ;
+                }
+            }
+        }
+    }
+}
---
+++ MyCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfL9
+{
+    class MyCommands
+    {
+        public static RoutedCommand Exit { get; set; }
+        public static RoutedCommand Bold { get; set; }
+        public static RoutedCommand Italic { get; set; }
+        public static RoutedCommand Underline { get; set; }
+        static MyCommands()
+        {
+            Exit = new RoutedUICommand("Выход", "Exit", typeof(MyCommands));
+            Bold = new RoutedCommand("Bold", typeof(MyCommands));
+            Italic = new RoutedCommand("Italic", typeof(MyCommands));
+            Underline= new RoutedCommand("Underline", typeof(MyCommands));
+        }
+    }
+
+}
